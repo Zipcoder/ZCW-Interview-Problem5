15e2652b851bfbe93642b2ef5785fcd4b476574f
Zachary <zstimmel@gmail.com>
2017-11-28 14:09:56 -0500

    zach prob 5 not working

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -2,14 +2,23 @@ package io.zipcoder;
 
 public class Problem5 {
 
-    public String reverseString(String inputString){
+    public String reverseString(String inputString) {
 
-        if(inputString.length()== 0){
-            return "";
+        if (inputString.equals("")) {
+            return inputString;
         }
+        
+        //string length gets 1 smaller every time method is called
 
-        return inputString.charAt(inputString.length()-1)+reverseString(inputString.substring(0,inputString.length()-1));
+        return inputString.charAt(inputString.length() - 1) + reverseString(inputString.substring(0, inputString.length() - 1));
     }
-    
+
+    public String allLowerCase(String inputString){
+        return inputString.toLowerCase();
+    }
+
+    public String captializeFirstChar(String inputString){
+        return Character.toUpperCase(inputString.charAt(0)) + allLowerCase(inputString.substring(1,inputString.length()));
+    }
 
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -10,9 +10,21 @@ public class Problem5Test {
         String input = "I love UNIT Testing";
         Problem5 test = new Problem5();
 
-        String expected = "Gnitset TINU Evol I";
+        String expected = "gnitseT TINU evol I";
 
         String actual = test.reverseString(input);
+        //only testing the reverse, no capitalization
+
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void capitalizeFirstCharTest(){
+        Problem5 test = new Problem5();
+        String input = test.reverseString("I love UNIT Testing");
+
+        String expected = "Gnitset TINU Evol I";
+
+        String actual = test.captializeFirstChar(input);
 
         Assert.assertEquals(expected,actual);
     }

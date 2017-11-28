e3dd2b8c18b05c72937712adedf12f21aa39e787
zan <zan@zipcoders-MacBook-Pro.local>
2017-11-28 10:39:59 -0500

    Zan- not passing test

--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
+import org.junit.*;
 
 public class Problem5Test {
+
+    @Test
+    public void reverseStringTest(){
+
+        String input = "I love UNIT Testing";
+
+        String expected = "Gnitest TINU Eovl I";
+
+        String actual = Problem5.reverseString();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+
 }

dce463986d4a6d859156d0ec4e9c8c02a42c0823
gpatelos <greg.patselas@gmail.com>
2017-11-28 10:39:16 -0500

    not reversed:(

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,14 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseString(String input){
+
+        for(int i = 0; i < input.length(); i++){
+
+            input.replace(input.subSequence(i, i),input.subSequence((input.length()-i)/2, (input.length()-i)/2));
+        }
+
+        return input;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseStringNoNewVariables(){
+        Problem5 problem5= new Problem5();
+        String input = "I love UNIT Testing";
+
+        String expected = "Gnitest TINU Eovl I";
+        String actual = problem5.reverseString(input);
+
+        Assert.assertEquals(expected,actual);
+
+
+    }
 }

dede0d4d4789b8c1756317122c1fc81b000301a7
pavel <Pavelparfenov112@gmail.com>
2017-11-28 10:39:14 -0500

    "Don't work"

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Problem5 {
+
+    //Don't work
+
+    public String reverseAndCapitalizeString(String input){
+        //step1 reverse the string
+        input = " "+input+" "; //add padding
+        for(int i = 0; i < input.length()/2; i++){
+            input = input.substring(0,i) + //padding + up to char to replace
+                    input.substring(i, input.length() -1 -i) + //char from end half
+                    input.substring(i+1, ) //middle
+            //char from beginning half
+            //end with padding
+        }
+        input = input.substring(1, input.length() - 1); //remove padding
+        //step2 capitalize every first letter in a word
+        input = Arrays.toString(Arrays.stream(input.split(" ")).
+                map(word -> word.replace(word.charAt(0), Character.toUpperCase((word.charAt(0))))).
+                map(word -> word + " ").
+                collect(Collectors.toList()).toArray());
+
+        return input;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseTest(){
+        Problem5 problem5 = new Problem5();
+        String input = "I love UNIT Testing";
+
+        String actual = problem5.reverseAndCapitalizeString(input);
+
+        String expected = "Gnitest TINU Eovl I";
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }

c2ca9fcb5f50c0ef4178d2557d00a59ece3a1806
Zachary <zstimmel@gmail.com>
2017-11-28 10:38:51 -0500

    zach prob 5 not working

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseString(String inputString){
+
+        inputString = String.valueOf(inputString.toCharArray());
+
+        for (int i = inputString.length()-1;i >= 0; i--){
+            inputString = inputString.toLowerCase();
+            //inputString.replace(inputString.charAt(inputString.length()-1))
+        }
+
+
+//          created new variables,cant use
+//
+//        StringBuilder output = new StringBuilder();
+//
+//        char [] chars = inputString.toLowerCase().toCharArray();
+//
+//        for (int i = chars.length-1;i >= 0;i--){
+//            output.append(chars[i]);
+//        }
+//
+//        return output.toString();
+        return inputString;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseStringTest(){
+        String input = "I love UNIT Testing";
+        Problem5 test = new Problem5();
+
+        String expected = "Gnitest TINU Eovl I";
+
+        String actual = test.reverseString(input);
+
+        Assert.assertEquals(expected,actual);
+    }
 }

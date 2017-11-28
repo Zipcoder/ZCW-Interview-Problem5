bc6b623b553c245fc7ad9d8ae32b77024c64516b
aaron <aaronherion111@gmail.com>
2017-11-28 10:56:12 -0500

    update

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -7,6 +7,8 @@ public class Problem5 {
     String reverseString(String str){
 
 
+        //hopefully the "i" in this loop doesn't count as a new variable
+        //not sure if the .add and .charAt are okay since they don't have to do with reversing but are built in nonetheless
 
         for (int i = str.length(); i >=0; i --){
             ArrayList<Character> letters = new ArrayList<Character>();

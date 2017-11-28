c56cf48ed4e61e808f4c70ea6503f2d8a3f29a4e
logan <loganhanner@gmail.com>
2017-11-28 10:39:29 -0500

    problem 5

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,48 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseAndCapitalize(String input) {
+        String[] splitInput = input.split("");
+        StringBuilder output = new StringBuilder();
+
+        int outputSetter = input.length() - 1;
+
+
+
+        for(int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(outputSetter);
+
+            if(i == 0) {
+                output.append(Character.toUpperCase(ch));
+            }
+            if(outputSetter >= 1) {
+                if(input.charAt(outputSetter - 1) == ' ') {
+                    ch = Character.toUpperCase(ch);
+                    output.append(ch);
+                }
+//                else if(input.charAt(outputSetter + 1) == ' ') {
+//                    ch = Character.toLowerCase(ch);
+//                    output.append(ch);
+//                }
+                else {
+                    output.append(ch);
+                }
+                outputSetter--;
+            }
+            else {
+                ch = input.charAt(0);
+                output.append(ch);
+            }
+
+//            while(outputSetter > 0) {
+//
+//            }
+
+        }
+
+        return output.toString();
+
+    }
+
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    Problem5 problem5;
+
+    @Before
+    public void setup() {
+        problem5 = new Problem5();
+    }
+
+    @Test
+    public void reverseAndCapitalize() throws Exception {
+
+        String input = "I love UNIT Testing";
+
+        String expected = "Gnitest TINU Eovl I";
+        String actual = problem5.reverseAndCapitalize(input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }

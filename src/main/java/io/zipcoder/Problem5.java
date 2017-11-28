753bd9778c38e4433dd84f8ebb3dee37f4abb13f
linda <linda@zipcoders-MacBook-Pro-8.local>
2017-11-28 14:01:04 -0500

    deleted some stuff

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -34,27 +34,5 @@ public class Problem5 {
 //            System.out.println(sb.charAt(i));
 //
 //    }
-//
-//
-//    public static String capitalize(String sL) {
-//
-//        StringBuilder sb = new StringBuilder();
-//        String result = "";
-//
-//        char currentChar;
-//
-//        for (int i = 0; i < stringLove.length(); i++) {
-//
-//             currentChar = stringLove.charAt(i);
-//
-//            if (i == 0) {
-//                result = result + Character.toUpperCase(currentChar);
-//            }
-//        }
-//return result;
-////        }
-////        return result.substring(0, result.length() - 1);
-//
-//    }
 //}
 

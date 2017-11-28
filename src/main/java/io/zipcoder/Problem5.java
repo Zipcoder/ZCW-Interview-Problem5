93241ac945368b669db6d4ef48b3044f13cc0447
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-28 10:40:29 -0500

    saving progress

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,40 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseStringWithCharacterArray(String input) {
+        char[] characterArray = input.toCharArray();
+        input = "";
+        for (int i = characterArray.length - 1; i >= 0; i--) {
+            input = input + characterArray[i];
+        }
+        return input;
+    }
+
+    public String reverseStringWithVariable(String input) {
+        String reverse = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse = reverse + input.charAt(i);
+        }
+        return reverse;
+    }
+
+    public String capitalizeFirstLetterOfEveryWord(String input) {
+        String capitalizedFirstLetterOFEveryWord = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (i == 0) {
+                capitalizedFirstLetterOFEveryWord = input.substring(i, i + 1).toUpperCase();
+            }
+            else if (input.substring(i, i + 1).equals(" ")) {
+                System.out.println("Character is a space");
+                capitalizedFirstLetterOFEveryWord += input.substring(i + 1, i + 2).toUpperCase();
+            }
+            else {
+                capitalizedFirstLetterOFEveryWord += input.substring(i, i + 1);
+            }
+            System.out.println("Character is " + input.charAt(i));
+        }
+        return capitalizedFirstLetterOFEveryWord;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseStringWithVariableTest() {
+        String forwardString = "I love andrew";
+        String backwardsExpected = "werdna evol I";
+        String actual = new Problem5().reverseStringWithVariable(forwardString);
+
+        Assert.assertEquals(backwardsExpected, actual);
+    }
+
+    @Test
+    public void reverseStringWithCharacterArrayTest() {
+        String forwardString = "I love andrew";
+        String backwardsExpected = "werdna evol I";
+        String actual = new Problem5().reverseStringWithCharacterArray(forwardString);
+
+        Assert.assertEquals(backwardsExpected, actual);
+    }
+
+    @Test
+    public void capitalizeFirstLetterOfEveryWordTest() {
+        String forwardString = "i love andrew";
+        String expectedString = "I Love Andrew";
+        String actual = new Problem5().capitalizeFirstLetterOfEveryWord(forwardString);
+
+        Assert.assertEquals(expectedString, actual);
+    }
 }

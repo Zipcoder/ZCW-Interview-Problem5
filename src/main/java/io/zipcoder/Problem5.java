83610aaa77355574455a515dd323cbd7bb89b2a2
lina <you@example.com>
2017-11-28 10:40:43 -0500

    didn't get cap yet

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+
+import java.util.regex.Pattern;
+
 public class Problem5 {
+
+    public String reverseString(String string){
+        String reverseStr="";
+        for(int i=string.length()-1;i>=0;i--){
+            reverseStr+=string.charAt(i);
+        }
+        return reverseStr;
+    }
+    public String capitalizeFirstL(String reverse){
+        String[] split = reverse.split(" ");
+        for(String each: split){
+            each.charAt(0).toUpperCase();
+        }
+
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+    Problem5 problem5 = new Problem5();
+    @Test
+    public void testReverseString(){
+        String string = "I love UNIT Testing";
+        String expected ="Gnitest TINU Eovl I";
+        String actual = problem5.reverseString(string);
+        Assert.assertEquals(expected,actual);
+    }
 }

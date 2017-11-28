e732f255464b6b443d9a6772df83fbe36e05776e
graham <grahamnilsen@gmail.com>
2017-11-28 10:39:38 -0500

    Reverse works

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    Problem5() {
+    }
+
+    public String reverseString(String input) {
+        if (input.length() == 0) {
+            return "";
+        }
+        return input.charAt(input.length() - 1) + reverseString(input.substring(0, input.length() - 1));
+
+    }
+
+    public String capitalizeString(String input) {
+        
+        return ;
+    }
+
+
 }

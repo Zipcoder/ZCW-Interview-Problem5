8f26b730405d2555ff07ad3c51ba6e7ca7def60b
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-28 10:39:39 -0500

    Ryan, I tried

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseStringAndCapitalizeFirstLetter(String string){
+        for (int i = string.length(); i >= 0; i--){
+            string += i;
+            System.out.print(string);//testing purposes
+        }
+        return string;
+
+    }
+
+    public String turnStringIntoArray(){
+
+    }
 }
+//no built in functions
+//no new variables????
+//no google
+
+//start at end of string and iterate backwards?...cant use .length()
+//not even close

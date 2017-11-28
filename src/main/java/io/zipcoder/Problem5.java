53c63653cc170abf7187747c2ebb0980defb180b
linda <linda@zipcoders-MacBook-Pro-8.local>
2017-11-28 10:40:25 -0500

    first commit

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,48 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Problem5 {
+
+
+    public static void main(String[] args) {
+        String input = "I love UNIT Testing";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+        sb = sb.reverse();
+
+        for (int i = 0; i < sb.length(); i++)
+            System.out.println(sb.charAt(i));
+
+    }
+
+
+    public static String reverse(String stringLove) {
+    return
+    }
 }
+
+
+//    public static String capitalize(String sL) {
+//
+//        StringBuilder sb = new StringBuilder();
+//        String result = "";
+//
+//        char currentChar;
+//
+//        for (int i = 0; i < stringLove.length(); i++) {
+//
+//             currentChar = stringLove.charAt(i);
+//
+//            if (i == 0) {
+//                result = result + Character.toUpperCase(currentChar);
+//            }
+//        }
+//return result;
+////        }
+////        return result.substring(0, result.length() - 1);
+//
+//    }
+//}
+
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,53 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    // Input: "I love UNIT Testing"
+    // Output: "Gnitest TINU Eovl I"
+
+    private String nothing = "";
+    private Problem5 problem;
+
+    @Before
+    public void setUp() {
+
+        problem = new Problem5();
+    }
+
+    @Test
+
+    public void reverse() {
+        String expected = " gnitesT TINU eovl I";
+        String actual = problem.reverse();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+      public void capitalizeFirstWord(){
+
+
+      }
+
+
+    @Test
+    public void noInputReverseTest(){
+        Boolean actual = Boolean.TRUE;
+        Boolean expected = problem.reverse(nothing);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+      public void noInputCapitalizeTest(){
+      Boolean actaul = Boolean.TRUE;
+      Boolean expected = problem.reverse(nothing);
+      Assert.assertEquals(expected, actual);
+}
+
+
 }

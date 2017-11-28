fd51bc0dd050c1533b57ff909285c1203b862b3f
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-11-28 10:39:18 -0500

    not even close

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
+import java.util.Collections;
+
 public class Problem5 {
+
+    public String reverseString(String str){
+        //Collections.reverse();
+//        Collections.reverse();
+//        str.toCharArray().
+
+
+// i is a new variable realized half way through
+//        str+=Character.toUpperCase(str.charAt(str.length()-1));
+//        for(int i = str.length()-3;i>=0;i--){
+//            System.out.println(str);
+//            System.out.println(str.charAt(i+1));
+//            if(str.charAt(i+1) == ' '){
+//                str+=Character.toUpperCase(str.charAt(i));
+//            }else{
+//                str+= str.charAt(i);
+//            }
+//        }
+//        return str.substring((str.length())/2);
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,17 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseTest(){
+        String input = "I love UNIT Testing";
+        String expected = "Gnitest TINU Eovl I";
+
+        String actual = new Problem5().reverseString(input);
+
+        Assert.assertEquals(expected,actual);
+    }
 }

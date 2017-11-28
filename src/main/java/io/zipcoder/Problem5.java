fb32aa60809df941f6465dfd70e5a5fe803b87b0
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-28 10:39:46 -0500

    did not do this right

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,13 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseTheString(String[] arr){
+        String output = "I love UNIT Testing";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            output += String.format("\n" + arr[i]);
+        }
+        return output;
+    }
 }
+//is output a new variable?? is that what the lab means? if so, i did this wrong.
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,17 @@
 package io.zipcoder;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Problem5Test {
-}
+
+    Problem5 problem5 = new Problem5();
+
+    @Test
+    public void reverseTheString(){
+        String[] arr = {"I love UNIT Testing"};
+        String expected = "Gnitest TINU Eovl I";
+        String actual = problem5.reverseTheString(arr);
+        Assert.assertEquals(expected,actual);
+    }
+}//not working

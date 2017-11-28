5c0206847054f4b359438c5d509748737dc125a6
aaron <aaronherion111@gmail.com>
2017-11-28 10:39:17 -0500

    Aaron Herion Submission

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,17 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Problem5 {
+
+    String reverseString(String str){
+
+
+
+        for (int i = str.length(); i >=0; i --){
+            ArrayList<Character> letters = new ArrayList<Character>();
+            str += letters.add(str.charAt(i));
+        }
+            return str;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseStringTest (){
+
+        Problem5 problem = new Problem5();
+
+        String expected = "Gnitest TINU Eovl I";
+
+        String actual = problem.reverseString("I love UNIT Testing");
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
 }

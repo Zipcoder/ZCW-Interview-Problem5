7adec78aba55b62558ab74ce34deeef8d487511c
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-28 10:39:34 -0500

    Katherine Anderson lol

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseString(String input) {
+//        String stringToReturn = "";
+//        for(int i = 0; i < input.length(); i++) {
+//            stringToReturn = input.charAt(i) + stringToReturn;
+//        }
+//        return stringToReturn;
+        return lastCharToBeginning(input, "");
+    }
+
+    private String lastCharToBeginning(String original, String current) {
+        if(original.length() > 0) {
+            return lastCharToBeginning(original.substring(1), original.substring(0, 1) + current);
+        }
+        else {
+            return current;
+        }
+    }
 }

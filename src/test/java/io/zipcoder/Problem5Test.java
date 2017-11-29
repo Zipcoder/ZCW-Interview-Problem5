cf5b352ac4c0b172fa49d896480bfcfa6e225f80
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-29 09:10:55 -0500

    re-did with different solution

--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,16 +1,24 @@
 package io.zipcoder;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class Problem5Test {
 
-    Problem5 problem5 = new Problem5();
+    Problem5 problem5;
+    String input;
+
+    @Before
+    public void setup() {
+        problem5 = new Problem5();
+        input = "I Love UNIT Testing";
+    }
 
     @Test
     public void reverseTheString(){
-        String expected = "gnitset TINU evol i";
-        String actual = problem5.reverseTheString("i love UNIT testing");
+        String expected = "gnitseT TINU evoL I";
+        String actual = problem5.reverseTheString(input);
         Assert.assertEquals(expected,actual);
     }
 }

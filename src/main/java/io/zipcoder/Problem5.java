e3f8c8f49ef4dd805299981bd92ffffe1bf45534
ahsonali <ahsonac@gmail.com>
2018-04-16 23:51:32 -0400

    more to write

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public String reverseThisWord(String text)
+    {
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--)
+        {
+            reversed += text.charAt(i);
+        }
+
+        return reversed.substring(0,1).toUpperCase() + reversed.substring(1, reversed.length());
+    }
+
+    public String reverseThisSentence(String sentence)
+    {
+        String reversedSentance = "";
+        String [] words = sentence.split(" ");
+
+        return null;
+    }
 }
+
+
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
-public class Problem5Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem5Test
+{
+
+    @Test
+    public void reverseThisStringTest1()
+    {
+        //Given
+        Problem5 problem5 = new Problem5();
+        String text = "hello";
+        String expected = "Olleh";
+
+        //When
+        String actual = problem5.reverseThisWord(text);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
 }

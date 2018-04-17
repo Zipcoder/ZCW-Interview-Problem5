370721ccfc2ec2adc648784c04d5cd5cfce873b7
ahsonali <ahsonac@gmail.com>
2018-04-17 11:47:10 -0400

    done for now

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -2,25 +2,48 @@ package io.zipcoder;
 
 public class Problem5 {
 
-    public String reverseThisWord(String text)
-    {
-        String reversed = "";
+    public String capitalizeWords(String sentence){
 
-        for (int i = text.length() - 1; i >= 0; i--)
+        //Capitalize first word of entire sentence
+        sentence = Character.toUpperCase(sentence.charAt(0)) + sentence.substring(1);
+
+        //Capitalizes each word by capitalizing the char one index after white space.
+        for (int i = 1; i < sentence.length(); i++)
         {
-            reversed += text.charAt(i);
+            if(sentence.charAt(i) == ' ')
+            {
+                sentence = sentence.substring(0, i + 1) +
+                           Character.toUpperCase(sentence.charAt(i + 1)) +
+                           sentence.substring(i + 2);
+            }
         }
 
-        return reversed.substring(0,1).toUpperCase() + reversed.substring(1, reversed.length());
+        return sentence;
     }
 
-    public String reverseThisSentence(String sentence)
+    public String reverseSentence(String sentence){
+
+        if (sentence.isEmpty()){
+            return sentence;
+        }
+
+        return reverseSentence(sentence.substring(1)) + sentence.charAt(0);
+
+    }
+
+    public String capitalAndReverse(String sentence)
     {
-        String reversedSentance = "";
-        String [] words = sentence.split(" ");
-
-        return null;
+        return capitalizeWords(reverseSentence(sentence));
     }
+
+
+
+
+
+
+
+
+
 }
 
 
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -3,24 +3,61 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Problem5Test
-{
+public class Problem5Test {
 
     @Test
-    public void reverseThisStringTest1()
-    {
+    public void capitalizeWordsTest1() {
         //Given
         Problem5 problem5 = new Problem5();
-        String text = "hello";
-        String expected = "Olleh";
+        String sentence = "I love Zip Code Wilmington cirriculum the best";
+        String expected = "I Love Zip Code Wilmington Cirriculum The Best";
 
         //When
-        String actual = problem5.reverseThisWord(text);
+        String actual = problem5.capitalizeWords(sentence);
 
         //Then
         Assert.assertEquals(expected, actual);
 
-
     }
+
+    @Test
+    public void capitalizeWordsTest2() {
+        //Given
+        Problem5 problem5 = new Problem5();
+        String sentence = "I love UNIT Testing";
+        String expected = "I Love UNIT Testing";
+
+        //When
+        String actual = problem5.capitalizeWords(sentence);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseSentence(){
+        //Given
+        Problem5 problem5 = new Problem5();
+        String sentence = "I love UNIT Testing";
+        String expected = "gnitseT TINU evol I";
+
+        String actual = problem5.reverseSentence(sentence);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void captialAndReverseTest(){
+        //Given
+        Problem5 problem5 = new Problem5();
+        String sentence = "I love UNIT Testing";
+        String expected = "GnitseT TINU Evol I";
+
+        String actual = problem5.capitalAndReverse(sentence);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
 
 }
